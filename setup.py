831261153581c6c1274c9e11c35b54a93717881f
phiresky <phireskyde+git@gmail.com>
2021-02-05 12:01:43 +0100

    yaml dependency

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name='gym_kilobots',
       version='0.0.1',
-      install_requires=['gym', 'box2d-py', 'numpy', 'scipy', 'pygame', 'matplotlib'],
+      install_requires=['gym', 'box2d-py', 'numpy', 'scipy', 'pygame', 'matplotlib', 'pyyaml'],
       packages=find_packages()
 )
